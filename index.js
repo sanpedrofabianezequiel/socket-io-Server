8b8755e74b940f20bbca8754adb2db55d58628ba
sanpedrofabianezequiel <sanpedrofabianezequiel@gmail.com>
2021-11-10 20:12:44 -0300

    Using the IO to send message to all clients

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 // Servidor de express
-const app = require('express')();
+const express = require('express');
+const app = express();
 //Servidor de sockets
 const server = require('http').createServer(app);
 //Configuration del sockets server
 const io = require('socket.io')(server);
 
-io.on('connection', () => { /* … */ });
+app.use( express.static( __dirname + '/public') );
+
+
+io.on('connection', (socket) => {
+    socket.emit('mensaje-bienvenida',{
+        msg:'Bienvenido al server',
+        fecha:new Date()
+    });
+
+    socket.on('mensaje-to-server',(data)=>{
+        console.log(data);
+        io.emit('mensaje-from-server',data)
+    })
+});
 
 server.listen(8080,()=>{
     console.log('Server corriendo en el puerto: 8080')
